cd5f9d694f1115fa344295ae963278c04d5ce8a4
Voles <niels.dequeker@gmail.com>
2014-01-13 10:33:27 +0100

    Add dummy data for the select-box example

--- example/hippo-theme-example.js
+++ example/hippo-theme-example.js
@@ -3,5 +3,14 @@
         
         .controller('SelectBoxCtrl', ['$scope', function ($scope) {
             console.log('Select box ctrl init!');
+            
+            $scope.selectedValues = [];
+            $scope.options = [
+                { "id": 1, "name": 'Option 1' },
+                { "id": 2, "name": 'Option 2' },
+                { "id": 3, "name": 'Option 3' },
+                { "id": 4, "name": 'Option 4' },
+                { "id": 5, "name": 'Option 5' }
+            ];
         }]);
 })();
