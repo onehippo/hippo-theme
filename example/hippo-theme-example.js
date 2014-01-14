d2ff3049eff527fd4a2ae021060f5826c46a47a3
Voles <niels.dequeker@gmail.com>
2014-01-14 09:52:27 +0100

    New build

--- example/hippo-theme-example.js
+++ example/hippo-theme-example.js
@@ -1,24 +1,24 @@
 (function () {
-    angular.module('hippo.theme.example', ['hippo.theme'])
-        
-        /**
-         * @ngdoc object
-         * @name hippo.theme.example:SelectBoxCtrl
-         *
-         * @description
-         * Controller for the Select Box example.
-         */
-        .controller('SelectBoxCtrl', ['$scope', function ($scope) {
-            console.log('Select box ctrl init!');
-            
-            $scope.selectedValues = [];
-            $scope.options = [
-                { "id": 1, "name": 'Option 1' },
-                { "id": 2, "name": 'Option 2' },
-                { "id": 3, "name": 'Option 3' },
-                { "id": 4, "name": 'Option 4' },
-                { "id": 5, "name": 'Option 5' }
-            ];
-            $scope.placeholder = "Choose an option";
-        }]);
+  angular.module('hippo.theme.example', ['hippo.theme'])
+    
+    /**
+     * @ngdoc object
+     * @name hippo.theme.example:SelectBoxCtrl
+     *
+     * @description
+     * Controller for the Select Box example.
+     */
+    .controller('SelectBoxCtrl', ['$scope', function ($scope) {
+      console.log('Select box ctrl init!');
+      
+      $scope.selectedValues = [];
+      $scope.options = [
+        { "id": 1, "name": 'Option 1' },
+        { "id": 2, "name": 'Option 2' },
+        { "id": 3, "name": 'Option 3' },
+        { "id": 4, "name": 'Option 4' },
+        { "id": 5, "name": 'Option 5' }
+      ];
+      $scope.placeholder = "Choose an option";
+    }]);
 })();
