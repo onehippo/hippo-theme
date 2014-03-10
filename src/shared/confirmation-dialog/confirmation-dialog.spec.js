abf45ee4502a7878e1e79911c0c2d30fe938d205
Voles <niels.dequeker@gmail.com>
2014-03-10 13:19:02 +0100

    CMS7-7775 Confirmation Dialog - added test to verify the directive is applied

---
+++ src/shared/confirmation-dialog/confirmation-dialog.spec.js
@@ -0,0 +1,23 @@
+describe('Hippo Theme confirmation dialog', function() {
+
+    beforeEach(function() {
+        // load modules
+        module('hippo.theme');
+    });
+
+    describe('the directive hippo.theme.confirmationDialog', function() {
+        var elm, scope;
+
+        beforeEach(inject(function($rootScope, $compile) {
+            scope = $rootScope;
+            elm = angular.element('<div hippo.theme.confirmation-dialog></div>');
+            $compile(elm)(scope);
+            scope.$digest();
+        }));
+
+        it('should exist', function() {
+            console.log(elm);
+            expect(elm).toHaveClass('confirmation-dialog');
+        });
+    });
+});
